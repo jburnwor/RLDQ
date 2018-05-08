036846bff56baf7e5eb17655a18cbefaa5df7b59
Yash Solanki <ysolanki@ucsc.edu>
2018-05-07 20:24:21 -0700

    Current Working build of alarm clock
    Bug with time tracking that needs to be ironed out

--- myGame/js/alarmClock.js
+++ myGame/js/alarmClock.js
@@ -3,11 +3,10 @@
 WebFontConfig = {
 
     //  'active' means all requested fonts have finished loading
-    //  We set a 1 second delay before calling 'createText'.
-    //  For some reason if we don't the browser cannot render the text the first time it's created.
-    active: function() { game.time.events.add(Phaser.Timer.SECOND, function(){}, this); },
+    //  We set a 2.5 second delay before calling 'createText' to make sure the browser can render it
+    active: function() { game.time.events.add(2500, function(){}, this); },
 
-    //  The Google Fonts we want to load (specify as many as you like in the array)
+    //  the font we want
     google: {
       families: ['Orbitron']
     }
@@ -16,45 +15,52 @@ WebFontConfig = {
 var alarmClock = function(){}
 alarmClock.prototype = {
 	preload: function(){
-		this.load.path = 'assets/img/alarmClock/';
-		this.load.image('clock','clockTemp.png');
-		this.load.image('finger','fingerTemp.png');
 		game.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
+		this.load.path = 'assets/img/alarmClock/';
+		this.load.image('clock','desk.png');
+		this.load.image('finger','fingerTemp.png');
+		this.load.image('bg','bg.png');
+		this.load.image('lamp','lamp.png');
+		this.load.image('button','masterButton.png');	
 	},
 	create: function(){
 		game.physics.startSystem(Phaser.Physics.ARCADE);	
 
 		//create 30 second timer for the stage
 		stageTimer = game.time.create(false);
-		stageTimer.add(30000,function(){console.log('fired')},game);
+		stageTimer.add(30000,this.checkTime,this);
 		stageTimer.start();
 
-		//to display the score
-		scoreDisplay = new Score();
+		
+		bg = this.add.sprite(0,0,'bg');
+
+
+		//add button to top of clock
+		button = this.add.sprite(game.world.centerX-50,game.world.centerY-65,'button');
+		game.physics.arcade.enable(button);
+		button.anchor.setTo(0.5,0.5);
+
+		confirmButton = this.add.sprite(game.world.centerX+50,game.world.centerY-65,'button');
+		game.physics.arcade.enable(confirmButton);
+		confirmButton.anchor.setTo(0.5,0.5);
+		confirmButton.inputEnabled = true;
+		confirmButton.events.onInputDown.add(this.checkTime,this);
 
 		//add clock
 		clock = this.add.sprite(game.world.centerX,game.world.centerY,'clock');
 		clock.anchor.setTo(0.5,0.5);
+
+		lamp = this.add.sprite(0,0,'lamp');
 		
-
-
-		//add button to top of clock
-		button = this.add.sprite(game.world.centerX,clock.y-90,'clock');
-		game.physics.arcade.enable(button);
-		button.tint = 0xff0000;
-		button.scale.setTo(0.50,0.25);
-		button.anchor.setTo(0.5,0.5);
-
-
 		//text for now
 		time = 0;
 		dayTrack = 0;
 		amPM = ['AM','PM'];
-		clockTime = this.add.text(clock.centerX+100,clock.centerY-64,"00:00   " + amPM[dayTrack%2],{font: 'Orbitron',fontSize: '32px', fill: 'White'});
+		clockTime = this.add.text(clock.centerX+110,clock.centerY-32,"00:00   " + amPM[dayTrack%2],{font: 'Orbitron',fontSize: '36px', fill: 'White'});
 		clockTime.anchor.setTo(1,0);
 
 		//finger
-		finger = this.add.sprite(game.world.centerX,0,'finger');
+		finger = this.add.sprite(game.world.centerX-50,0,'finger');
 		finger.anchor.setTo(0.5,0.5);
 		finger.inputEnabled = true; 				//Enables input (mouse click) for the finger
 		//it can be dragged arround by the mouse, setting the value changes if the object will snap to center of mouse
@@ -71,6 +77,9 @@ alarmClock.prototype = {
 		//since the event doesn't start the first time we'll just trigger it manually
 		doOnce = true;
 
+		//to display the score
+		scoreDisplay = new Score();
+		
 
 	},
 	update: function(){
@@ -82,36 +91,64 @@ alarmClock.prototype = {
 
 	},
 	render: function(){
-		game.debug.body(button);
-		game.debug.body(finger);
-		game.debug.geom(fingerBounds);
+		//game.debug.body(button);
+		//game.debug.body(finger);
+		//game.debug.geom(fingerBounds);
 	},
 
 	updateClock: function(){
 		if(!overlapped){
-			if(time>12){
-				time = time%12;
-				dayTrack++;
+			var wrapped;
+			overlapped = true;
+			if(!doOnce){
+					time = time + game.rnd.integerInRange(1,4);		
+			}
+			if(time==12){
+					dayTrack++;
+					wrapped = true;
+			}
+			else if(time>12){
+					if(!wrapped){
+						dayTrack++;
+					}
+					time = time%12;
+					wrapped = false;
 			}
 			if(time<10){
 				clockTime.text = "0"+time+":00   " + amPM[dayTrack%2];
+				console.log(time);
 			}
 			else{
 				clockTime.text = time +":00   " + amPM[dayTrack%2];
+				console.log(time);
 			}
-			time = time + game.rnd.integerInRange(1,4);
-			overlapped = true;
 		}
 	},
 
 	buttonPress: function(){
 		if(game.physics.arcade.collide(finger,button)){
+			
+
 			this.updateClock();
-			//button.body.y+=15;
+
+	
 		}
 		else if(!game.physics.arcade.collide(finger,button)){
 			overlapped = false;
-			//button.body.y-=25;
+			
+		}
+	},
+
+	checkTime: function(){
+		if((time == 8) && dayTrack%2 == 0){
+			console.log('right time' + time +' '+amPM[dayTrack%2]);
+			score+=50;
+			game.state.start('stamping');
+		}
+		else{
+			console.log('wrong time' + time +' '+amPM[dayTrack%2]);
+			health-=10;
+			game.state.start('stamping');
 		}
 	}
 }
