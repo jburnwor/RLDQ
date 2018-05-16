2b6fb36846639c1fd40e7f22207f74c1da9b0afc
Yash Solanki <ysolanki@ucsc.edu>
2018-05-16 12:35:29 -0700

    Forgot to change this back to menu

--- myGame/js/main.js
+++ myGame/js/main.js
@@ -12,5 +12,5 @@ window.onload = function () {
 	game.state.add('alarmClock',alarmClock);
 	game.state.add('bed',bed);	
 	game.state.add('menu', menu);
-	game.state.start('alarmClock');
+	game.state.start('menu');
 }
