6b9c61fc1946a91b1d95583bf0a2616416b59385
Yash Solanki <ysolanki@ucsc.edu>
2018-05-18 21:57:18 -0700

    Current Working Coffee State

--- myGame/js/pourCoffee.js
+++ myGame/js/pourCoffee.js
@@ -1,5 +1,5 @@
 var coffee = function () {
-	this.gumsEmitter;
+		this.coffeeEmitter
 }
 coffee.prototype = {
 	preload: function () {
@@ -8,26 +8,78 @@ coffee.prototype = {
 	},
 
 	create: function () {
+
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+
 		//timer for the stage
 		stageTimer = game.time.create(false);
 		stageTimer.add(30000, function(){console.log('timer')}, game);
 		stageTimer.start();
 
 		coffeeJug = this.add.sprite(game.world.centerX,0,'player');
+		game.physics.arcade.enable(coffeeJug);
+		coffeeJug.scale.setTo(3,3);
+		coffeeJug.tint = 4169e1;
+		coffeeJug.drag = 15;
+		coffeeJug.body.collideWorldBounds = true;
 		coffeeJug.anchor.setTo(0.5,0);
 
-		this.gumsEmitter = game.add.emitter(coffeeJug.x, 0, 200);
-		this.gumsEmitter.makeParticles('player');			// image used for particles
-		this.gumsEmitter.gravity = 200;
-		this.gumsEmitter.setXSpeed(0,0);
-		this.gumsEmitter.setYSpeed(100,100);
-		this.gumsEmitter.setRotation(0,0);
-		this.gumsEmitter.start(false, 10000, null, 75);	// (explode, lifespan, freq, quantity)
+		jugVelocity = game.time.create(false);
+		jugVelocity.loop(1000, this.jugSpeed, game);
+		jugVelocity.start();
+
+		coffeeEmitter = game.add.emitter(coffeeJug.x, 0, 0);
+		coffeeEmitter.gravity = 800;
+		coffeeEmitter.setXSpeed(0,0);
+		coffeeEmitter.setYSpeed(500,500);
+		coffeeEmitter.setRotation(0,0);
+		coffeeEmitter.makeParticles('player',0,4000,true,false);
+		coffeeEmitter.start(false, 3000, 25);	// (explode, lifespan, freq, quantity)
+
+
+		mug = this.add.sprite(game.world.centerX,game.world.height-96,'player');
+		game.physics.arcade.enable(mug);
+		mug.scale.setTo(2,3);
+		//setSize(width, height,relative X, relative Y)
+		mug.body.setSize(mug.width/2, 1, 0, 0);
+		mug.body.immovable = true;
+		mug.anchor.setTo(0.5,0);
+		count = 0;
+
+		scoreDisplay = new Score();
+
 	},
 	update: function () {
 		//move brush to pointer
-		coffeeJug.x = this.game.input.mousePointer.x;
+		mug.x = this.game.input.mousePointer.x;
+		coffeeEmitter.x = coffeeJug.x;
+
+		//calls the function on each particle/child
+		coffeeEmitter.forEach(this.checkCollision, this);
+		
+
 	},
+	jugSpeed: function() {
+		if(coffeeJug.x == coffeeJug.width/2){
+			coffeeJug.body.velocity.x = game.rnd.integerInRange(180,500);
+		}
+		else if(coffeeJug.x == (game.world.width-coffeeJug.width/2)){
+			coffeeJug.body.velocity.x = game.rnd.integerInRange(-500,-180);
+		}
+		else{
+			coffeeJug.body.velocity.x = game.rnd.integerInRange(-500,500);
+		}
+	},
+	checkCollision: function(particle){
+		if(game.physics.arcade.collide(particle,mug)){
+			particle.kill();
+			count++;
+			if(count%10==0){
+				score+=2;
+			}
+		}		
+	},
+
 }
 
 
